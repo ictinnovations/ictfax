6ab90be928c1529a0e24ae64686f9812d7b3b6ef
Fiza Khan <fiza@localhost.localdomain>
2019-03-02 13:51:30 +0500

    Remove the service_flag parameter while creating a provider

--- src/app/pages/provider/provider.ts
+++ src/app/pages/provider/provider.ts
@@ -2,7 +2,7 @@ export class Provider {
   provider_id: number;
   name: string;
   gateway_flag: number;
-  service_flag: number;
+  // service_flag: number;
   node_id: number;
   host: number;
   port: number;
